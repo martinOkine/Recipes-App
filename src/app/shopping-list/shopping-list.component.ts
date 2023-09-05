2bffdcc558c64efc9534c1bbdc24c0fb1503ef5d
martin <martin.okine@gmail.com>
2023-09-05 10:18:22 +0000

    added recipe list, recipe detail, shopping list and shopping list edit components pending functionality

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+  ingredients = [
+    new Ingredient('tomatoes', 6),
+    new Ingredient('apples', 5),
+    new Ingredient('banana', 4),
+    new Ingredient('eggs', 12),
+    new Ingredient('onions', 4),
+  ];
 }
